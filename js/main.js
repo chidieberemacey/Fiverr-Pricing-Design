4dc31451df8b10cbd1a22fc14afa364df808a81b
Chidiebere Macey <egbomaceyskyla@gmail.com>
2023-09-06 11:21:16 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let upper = document.querySelector('.upper');
+document.querySelector('.upper').onclick = run;
+
+function run(){
+    // upper.style.borderBottom = ' 3px solid rgb(61, 202, 80)'
+    // upper.style.color = 'rgb(61, 202, 80)'
+    for(let i = 0; i < top.length; i++){
+        top[i].style.borderBottom = ' 3px solid rgb(61, 202, 80)'
+        top[i].style.color = 'rgb(61, 202, 80)'
+    }
+}
+
+
+
+
+
